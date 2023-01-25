9d253c0effe79bd8d78de0158778b13817833723
mulonemartin <mulone.martin@gmail.com>
2023-01-25 16:08:04 -0300

    feat: roc deploy

---
+++ README.md
@@ -0,0 +1,83 @@
+# ROC protocol deploy
+
+ROC is an implementation of the Money On Chain main protocol that uses RIF as collateral of the system.
+For more information, please refer to the [documentation section](https://github.com/money-on-chain/main-sc-protocol/blob/master/docs/README.md).
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+What things you need to install the software and how to install them
+
+```bash
+# Install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+# Install proper node version
+nvm use
+```
+
+Create `.env` file (you can base on [`.env.example`](./.env.example))
+
+### Installing
+
+A step by step series of examples that tell you how to get a development env running
+
+Say what the step will be
+
+```bash
+# Install the dependencies
+npm install
+```
+
+### Generate Types
+
+In order to get contract types
+
+```bash
+npm run typechain
+```
+
+## Deployment
+
+This solution has a fully functional deploy mechanism following [hardhat deploy](https://github.com/wighawag/hardhat-deploy) standard.
+It imports smart contracts and deploy scripts from [MoC-main-sc](https://github.com/money-on-chain/main-sc-protocol) repository and uses them to deploy the collateral bag implementation with custom networks configurations. To add networks for deployment or set new deploy parameters see [hardhat.base.config.ts](hardhat.base.config.ts)
+
+You can configure a network as `testnet` using [tags](https://github.com/wighawag/hardhat-deploy#tags) mechanism, that will allow you to:
+1. deploy and initialize the protocol using a governor mocked to skip governance system
+2. initialize the protocol with Pegged Tokens
+3. initialize the protocol with Assets to mocWrapper
+4. transfer governance to the real governor
+
+Otherwise, you can set it as `mainnet` and will be initialized with the real governor. In that way, you must add Pegged Tokens and Assets using the governance system. See more in [Areopagus-Governance](https://github.com/money-on-chain/Areopagus-Governance)
+
+### Deploy scripts
+
+To deploy contracts in rsk testnet
+```bash
+npm run deploy-rskTestnet
+```
+
+A deployments folder will be created with a sub folder for each network. There you will find a json file for each contract deployed that includes the address and ABI.
+
+Keep in mind that all the contracts are upgradeable using [UUPS](https://eips.ethereum.org/EIPS/eip-1822) proxy pattern, so to interact with them you must use the contract implementation ABI but calling the contract proxy address.
+## Built With
+
+* [Hardhat](https://hardhat.org/) - Task runner
+
+## Contributing
+
+Please read [CONTRIBUTING.md](./CONTRIBUTING.md) for details on our code of conduct, and the process for submitting pull requests.
+
+## Versioning
+
+We use [SemVer](http://semver.org/) and [conventional commits](https://www.conventionalcommits.org/en/v1.0.0/) for versioning. For the versions available, see the [tags on this repository](https://github.com/your/project/tags).
+
+To create a new release execute the script
+
+`npm run release`
+
+## License
+
+See the [LICENSE](./LICENSE) file for details
---
+++ hardhat.base.config.ts
@@ -0,0 +1,234 @@
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-solhint";
+import "@nomicfoundation/hardhat-chai-matchers";
+import "@openzeppelin/hardhat-upgrades";
+import "@typechain/hardhat";
+import { resolve } from "path";
+import { config as dotenvConfig } from "dotenv";
+import "hardhat-contract-sizer";
+import "hardhat-deploy";
+import "hardhat-docgen";
+import "hardhat-gas-reporter";
+import { removeConsoleLog } from "hardhat-preprocessor";
+import { HardhatUserConfig } from "hardhat/config";
+import { BigNumber } from "ethers";
+import "solidity-coverage";
+import "hardhat-storage-layout";
+import { DeployParameters } from "moc-main/export/types/types";
+
+dotenvConfig({ path: resolve(__dirname, "./.env") });
+
+const PCT_BASE = BigNumber.from((1e18).toString());
+const DAY_BLOCK_SPAN = 2880;
+const MONTH_BLOCK_SPAN = DAY_BLOCK_SPAN * 30;
+declare module "hardhat/types/config" {
+  export interface HardhatNetworkUserConfig {
+    deployParameters: DeployParameters;
+  }
+  export interface HardhatNetworkConfig {
+    deployParameters: DeployParameters;
+  }
+  export interface HttpNetworkConfig {
+    deployParameters: DeployParameters;
+  }
+}
+
+const chainIds = {
+  ganache: 1337,
+  goerli: 5,
+  hardhat: 31337,
+  kovan: 42,
+  mainnet: 1,
+  rinkeby: 4,
+  ropsten: 3,
+  rskTestnet: 31,
+  polygonMumbai: 80001,
+};
+
+// Ensure that we have all the environment variables we need.
+let mnemonic: string;
+if (!process.env.MNEMONIC) {
+  throw new Error("Please set your MNEMONIC in a .env file");
+} else {
+  mnemonic = process.env.MNEMONIC;
+}
+/*
+let alchemyApiKey: string;
+if (!process.env.ALCHEMY_API_KEY) {
+  throw new Error("Please set your ALCHEMY_API_KEY in a .env file");
+} else {
+  alchemyApiKey = process.env.ALCHEMY_API_KEY;
+}*/
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  namedAccounts: {
+    deployer: 0,
+    otherUser: 1,
+    alice: 2,
+    bob: 3,
+    charlie: 4,
+  },
+  networks: {
+    hardhat: {
+      accounts: {
+        mnemonic,
+        accountsBalance: "100000000000000000000000000000000000",
+      },
+      chainId: chainIds.hardhat,
+      deployParameters: {
+        coreParams: {
+          protThrld: PCT_BASE.mul(2), // 2
+          liqThrld: PCT_BASE.mul(104).div(100), // 1.04
+          emaCalculationBlockSpan: DAY_BLOCK_SPAN,
+          successFee: PCT_BASE.mul(10).div(100), // 10%
+          appreciationFactor: PCT_BASE.mul(50).div(100), // 50%
+        },
+        settlementParams: {
+          bes: MONTH_BLOCK_SPAN,
+        },
+        feeParams: {
+          feeRetainer: PCT_BASE.mul(0), // 0%
+          mintFee: PCT_BASE.mul(5).div(100), // 5%
+          redeemFee: PCT_BASE.mul(5).div(100), // 5%
+          swapTPforTPFee: PCT_BASE.mul(1).div(100), // 1%
+          swapTPforTCFee: PCT_BASE.mul(1).div(100), // 1%
+          swapTCforTPFee: PCT_BASE.mul(1).div(100), // 1%
+          redeemTCandTPFee: PCT_BASE.mul(8).div(100), // 8%
+          mintTCandTPFee: PCT_BASE.mul(8).div(100), // 8%
+        },
+        ctParams: {
+          name: "CollateralToken",
+          symbol: "CT",
+        },
+        mocAddresses: {
+          governorAddress: "0x26a00af444928d689dDEc7B4D17C0e4A8c9D407A",
+          pauserAddress: "0x26a00aF444928D689DDec7B4D17C0e4a8c9d407b",
+          mocFeeFlowAddress: "0x26a00aF444928d689DDEC7b4D17c0E4a8c9D407d",
+          mocAppreciationBeneficiaryAddress: "0x26A00aF444928D689ddEC7B4D17C0E4A8C9d407F",
+        },
+      },
+      tags: ["local"],
+    },
+    rskTestnet: {
+      accounts: [`0x${process.env.MNEMONIC}`],
+      chainId: chainIds.rskTestnet,
+      url: "https://public-node.testnet.rsk.co",
+      deployParameters: {
+        coreParams: {
+          protThrld: PCT_BASE.mul(15).div(10), // 1.5
+          liqThrld: PCT_BASE.mul(104).div(100), // 1.04
+          emaCalculationBlockSpan: DAY_BLOCK_SPAN,
+          successFee: PCT_BASE.mul(20).div(100), // 20%
+          appreciationFactor: PCT_BASE.mul(0).div(100), // 0%
+        },
+        settlementParams: {
+          bes: DAY_BLOCK_SPAN,
+        },
+        feeParams: {
+          feeRetainer: PCT_BASE.div(10), // 10%
+          mintFee: PCT_BASE.div(1000), // 0.1%
+          redeemFee: PCT_BASE.div(1000), // 0.1%
+          swapTPforTPFee: PCT_BASE.div(1000), // 0.1%
+          swapTPforTCFee: PCT_BASE.div(1000), // 0.1%
+          swapTCforTPFee: PCT_BASE.div(1000), // 0.1%
+          redeemTCandTPFee: PCT_BASE.div(1000), // 0.1%
+          mintTCandTPFee: PCT_BASE.div(1000), // 0.1%
+        },
+        ctParams: {
+          name: "RIFPRO",
+          symbol: "RIFP",
+        },
+        tpParams: {
+          tpParams: [
+            {
+              name: "RIFUSD",
+              symbol: "RUSD",
+              priceProvider: "0x0e8E63721E49dbde105a4085b3D548D292Edf38A".toLowerCase(),
+              ctarg: PCT_BASE.mul(55).div(10), // 5.5
+              mintFee: PCT_BASE.div(100), // 1%
+              redeemFee: PCT_BASE.div(100), // 1%
+              initialEma: PCT_BASE.mul(6790).div(100000), //0.06790
+              smoothingFactor: PCT_BASE.mul(1104).div(100000), // 0.01104
+            },
+          ],
+        },
+        assetParams: {
+          assetParams: [
+            {
+              assetAddress: "0x19F64674D8A5B4E652319F5e239eFd3bc969A1fE".toLowerCase(),
+              priceProvider: "0x9d4b2c05818A0086e641437fcb64ab6098c7BbEc".toLowerCase(),
+              decimals: 18,
+            },
+          ],
+        },
+        mocAddresses: {
+          governorAddress: "0xf984d6f2afcf057984034ac06f2a2182cb62ce5c",
+          pauserAddress: "0x94b25b38DB7cF2138E8327Fc54543a117fC20E72",
+          mocFeeFlowAddress: "0xcd8a1c9acc980ae031456573e34dc05cd7dae6e3",
+          mocAppreciationBeneficiaryAddress: "0xcd8a1c9acc980ae031456573e34dc05cd7dae6e3",
+        },
+      },
+      tags: ["testnet"],
+    },
+  },
+  paths: {
+    artifacts: process.env.DEPLOYING ? "node_modules/moc-main/export/artifacts" : "./artifacts",
+    cache: "./cache",
+    sources: "./contracts",
+    tests: "./test",
+  },
+  solidity: {
+    version: "0.8.16",
+    settings: {
+      // https://hardhat.org/hardhat-network/#solidity-optimizer-support
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+      viaIR: process.env.VIA_IR ? true : false,
+      outputSelection: {
+        "*": {
+          "*": ["storageLayout"],
+        },
+      },
+    },
+  },
+  typechain: {
+    outDir: "typechain",
+    target: "ethers-v5",
+    alwaysGenerateOverloads: false,
+    externalArtifacts: ["node_modules/moc-main/export/artifacts/*.json"],
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS ? true : false,
+    currency: "USD",
+    gasPrice: 21,
+  },
+  preprocess: {
+    eachLine: removeConsoleLog(hre => !["hardhat", "localhost"].includes(hre.network.name)),
+  },
+  docgen: {
+    path: "./docs",
+    clear: true,
+    runOnCompile: false,
+    except: ["^contracts/echidna/", "^contracts/mocks/"],
+  },
+  contractSizer: {
+    alphaSort: true,
+    runOnCompile: true,
+    disambiguatePaths: false,
+  },
+  mocha: {
+    timeout: 100000,
+  },
+  external: {
+    contracts: [
+      {
+        artifacts: "node_modules/moc-main/export/artifacts",
+        deploy: "node_modules/moc-main/export/deploy/deploy/rc20",
+      },
+    ],
+  },
+};
+
+export default config;
